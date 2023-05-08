1f13b2063c4831b91e1f24f82e779db8c5eefd62
Cameron Hill <cameronhill347@gmail.com>
2023-05-08 15:43:14 +0100

    $(J) clicking a profile picture in the feed takes you to the author profile

--- src/components/postView.tsx
+++ src/components/postView.tsx
@@ -11,13 +11,15 @@ export const PostView = (props: PostWithUser) => {
   const { post, author } = props;
   return (
     <div className="flex gap-3 border-b border-slate-400 p-4" key={post.id}>
-      <Image
-        className="h-12 w-12 rounded-full"
-        src={author.profileImageUrl ?? "/default-profile-image.png"}
-        alt="profile image"
-        width={48}
-        height={48}
-      />
+      <Link href={`/@${author.name}`}>
+        <Image
+          className="h-12 w-12 rounded-full"
+          src={author.profileImageUrl ?? "/default-profile-image.png"}
+          alt="profile image"
+          width={48}
+          height={48}
+        />
+      </Link>
       <div className="flex flex-col">
         <div className="flex gap-1 text-slate-300">
           <Link href={`/@${author.name}`}>

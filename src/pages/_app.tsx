9125a1546cc1843e9842d92afe369177699f964e
Cameron-Hill <cameronhill347@gmail.com>
2023-05-17 08:37:38 +0100

    add open graph metadata

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,12 +7,26 @@ import "~/styles/globals.css";
 import { Toaster } from "react-hot-toast";
 import Head from "next/head";
 
+const openGraphData = {
+  title: "Cams's Chirp",
+  description:
+    "Chirp is a tutorial project for create-t3-app. You can find the tutorial source here: \
+    https://www.youtube.com/watch?v=YkOSUVzOAA4&ab_channel=Theo-t3%E2%80%A4gg",
+  url: "https://chirp.cameronhill.co.uk/",
+  image:
+    "https://cameron-hill-profile-images.s3.eu-west-2.amazonaws.com/page_preview_image.png",
+};
+
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider {...pageProps}>
       <Head>
         <title>Chirp</title>
-        <meta name="description" content="📧" />
+        <meta property="og:title" content={openGraphData.title} />
+        <meta property="og:description" content={openGraphData.description} />
+        <meta property="og:url" content={openGraphData.url} />
+        <meta property="og:image" content={openGraphData.image} />
+        <meta name="twitter:card" content="summary" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Toaster position="bottom-center" />
